42858a6569848d86129db3269c5a0ea4adfcbf11
Douglas DeJong <dougdejong@gmail.com>
2023-07-04 15:13:06 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Pedometer } from "expo-sensors";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [pedometerAvailability, setPedometerAvailability] = useState("");
+	const [stepCount, updateStepCount] = useState(0);
+
+	useEffect(() => {
+		subscribe();
+	}, []);
+	subscribe = () => {
+		const subscription = Pedometer.watchStepCount((result) => {
+			updateStepCount(result.steps);
+		});
+	};
+	Pedometer.isAvailableAsync().then(
+		(result) => {
+			setPedometerAvailability(String(result));
+		},
+		(error) => {
+			setPedometerAvailability(error);
+		}
+	);
+
+	return (
+		<View style={styles.container}>
+			{/* <Text style={styles.headingDesign}>
+				Is the Pedometer Availabe? : {pedometerAvailability}
+			</Text> */}
+			{/* <Text style={styles.headingDesign}>Current Steps: {stepCount}</Text> */}
+			<View>
+				<CircularProgress
+					value={stepCount}
+					maxValue={10000}
+					radius={210}
+					textcolor="white"
+					activeStrokeColor="green"
+					inActiveStrokeColor="grey"
+					inActiveStrokeOpacity={2.5}
+					inactiveStrokeWidth={60}
+					title={"Hot Girl Steps"}
+					titleColor="white"
+					titleStyle={{ fontSize: 30, fontWeight: "bold" }}
+				></CircularProgress>
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "black",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	headingDesign: {
+		color: "white",
+		fontSize: 24,
+	},
 });
